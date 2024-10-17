a161606a7e9c9912025bcd32385086fa412b0ed5
maia <matteusmaia889@gmail.com>
2024-10-17 17:54:16 -0300

    feat: Integration Form with Emailjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,29 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form')
+    contactMessege = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
 
+    emailjs.send('service_iwv20oc', 'template_9e55tsh','#contact-form', 'jC_iAK5wdMrBaG9o3')
+    .then(() => {
+        // Show sent message
+        contactMessege.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() => {
+            contactMessege.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+    }, () => {
+        // Show error message
+        contactMessege.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener("submit", sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
 
 
